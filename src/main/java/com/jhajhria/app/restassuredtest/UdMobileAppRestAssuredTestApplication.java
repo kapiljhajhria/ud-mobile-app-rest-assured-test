f9ae8afc0b8048b94ece194769508ca49b9ffb30
kapil jhajhria <6892756+kapiljhajhria@users.noreply.github.com>
2022-03-25 21:39:58 +0530

    init

---
+++ src/main/java/com/jhajhria/app/restassuredtest/UdMobileAppRestAssuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.jhajhria.app.restassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdMobileAppRestAssuredTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdMobileAppRestAssuredTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhajhria/app/restassuredtest/UdMobileAppRestAssuredTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhajhria.app.restassuredtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdMobileAppRestAssuredTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
